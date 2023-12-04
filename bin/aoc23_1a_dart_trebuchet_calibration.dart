7142eccd6dce373c6f372c09d39eac153b9c2397
Mark Sheppard <mark_sheppard_1977@hotmail.com>
2023-12-04 14:59:02 +0000

    working

--- bin/aoc23_1a_dart_trebuchet_calibration.dart
+++ bin/aoc23_1a_dart_trebuchet_calibration.dart
@@ -1,5 +1,25 @@
-import 'package:aoc23_1a_dart_trebuchet_calibration/aoc23_1a_dart_trebuchet_calibration.dart' as aoc23_1a_dart_trebuchet_calibration;
+import 'dart:async';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${aoc23_1a_dart_trebuchet_calibration.calculate()}!');
+void main(List<String> arguments) async {
+  // https://adventofcode.com/2023/day/1/input
+  var input = await File('assets/input.txt')
+      .readAsString()
+      .then((value) => value.split('\n'));
+  print(input);
+  var sum = 0;
+  for (var element in input) {
+    var nums = <int>[];
+    for (var char in element.split('')) {
+      var tmp = int.tryParse(char);
+      if (tmp != null) nums.add(tmp);
+    }
+    final first = nums.first;
+    final last = nums.last;
+    final calibCode = int.parse('$first$last');
+    sum += calibCode;
+    print('$element --> ${nums.join()} --> $calibCode --> $sum');
+  }
+  print(
+      "\n${''.padLeft(21, '-')}\nThe answer is '$sum'\n${''.padLeft(21, '-')}");
 }
